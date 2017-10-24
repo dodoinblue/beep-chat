09e48d73d169cac54bbd48bbc795eb5d1f8ace2e
Charles Liu <dodoinblue@gmail.com>
2017-10-23 21:27:00 -0700

    Login component

---
+++ src/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { LoginFormComponent } from './login-form/login-form.component';
+@NgModule({
+	declarations: [LoginFormComponent],
+	imports: [IonicModule],
+	exports: [LoginFormComponent]
+})
+export class ComponentsModule {}
