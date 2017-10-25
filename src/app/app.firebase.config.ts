cec6dcb31291fa6ac1fb34f7129b4be9da1ba2dc
Charles Liu <dodoinblue@gmail.com>
2017-10-24 19:48:28 -0700

    Add angularfire dependency

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA5Bngrmb5nWw2_IpqmwVoS1eKdXZY8T9s",
+  authDomain: "beep-50b03.firebaseapp.com",
+  databaseURL: "https://beep-50b03.firebaseio.com",
+  projectId: "beep-50b03",
+  storageBucket: "",
+  messagingSenderId: "1074210787704"
+}
