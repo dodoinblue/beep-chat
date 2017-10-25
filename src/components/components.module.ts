d27310edfca5f9bcd95c802832efdbab7ad1bf93
Charles Liu <dodoinblue@gmail.com>
2017-10-24 19:44:13 -0700

    Register form component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 @NgModule({
-	declarations: [LoginFormComponent],
+	declarations: [LoginFormComponent,
+    RegisterFormComponent],
 	imports: [IonicModule],
-	exports: [LoginFormComponent]
+	exports: [LoginFormComponent,
+    RegisterFormComponent]
 })
 export class ComponentsModule {}
---
+++ src/components/register-form/register-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: 'register-form.component.html'
+})
+export class RegisterFormComponent {
+
+  constructor() {
+  }
+
+  register() {}
+
+}
