1f094dafbef68926777eb387ea619e3f72e01fd4
Charles Liu <dodoinblue@gmail.com>
2017-10-24 22:17:56 -0700

    login function

--- src/components/login-form/login-form.component.ts
+++ src/components/login-form/login-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Account } from '../../models/account/account.interface';
 
 @Component({
   selector: 'app-login-form',
@@ -7,10 +9,21 @@ import { NavController } from 'ionic-angular';
 })
 export class LoginFormComponent {
 
-  text: string;
+  account = {} as Account;
 
-  constructor(private navCtrl: NavController) {
+  constructor(private afAuth: AngularFireAuth, private navCtrl: NavController) {
   }
+
+  async login() {
+    console.log('login')
+    try {
+      const result = await this.afAuth.auth.signInWithEmailAndPassword(this.account.email, this.account.password);
+      console.log('login success')
+    } catch(e) {
+      console.log('login error: ' + e.message)
+    }
+  }
+
 
   navigateToPage(pageName: string) {
     pageName === 'TabsPage' ? this.navCtrl.setRoot(pageName) : this.navCtrl.push(pageName);
