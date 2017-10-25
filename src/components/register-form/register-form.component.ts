8944beb75aed8ac5f488403874af8ed2cd530e75
Charles Liu <dodoinblue@gmail.com>
2017-10-24 21:06:24 -0700

    register function

--- src/components/register-form/register-form.component.ts
+++ src/components/register-form/register-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Account } from '../../models/account/account.interface';
+import { ToastController } from 'ionic-angular'
 
 @Component({
   selector: 'app-register-form',
@@ -6,9 +9,26 @@ import { Component } from '@angular/core';
 })
 export class RegisterFormComponent {
 
-  constructor() {
+  account = {} as Account;
+
+  constructor(private afAuth: AngularFireAuth, private toast: ToastController) {
   }
 
-  register() {}
+  async register() {
+    try {
+      const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.account.email, this.account.password)
+      console.log(result);
+      this.toast.create({
+        message: "Account created",
+        duration: 3000
+      }).present();
+    } catch (e) {
+      console.log(e);
+      this.toast.create({
+        message: e.message,
+        duration: 3000
+      }).present();
+    }
+  }
 
 }
