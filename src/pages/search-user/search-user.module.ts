b3e9326aaa7d471cd6fedf062ddc1f2014acfae6
Charles Liu <dodoinblue@gmail.com>
2017-12-05 00:05:02 -0800

    Search user component and update firebase db rules

--- src/pages/search-user/search-user.module.ts
+++ src/pages/search-user/search-user.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SearchUserPage } from './search-user';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { SearchUserPage } from './search-user';
   ],
   imports: [
     IonicPageModule.forChild(SearchUserPage),
+    ComponentsModule
   ],
 })
 export class SearchUserPageModule {}
