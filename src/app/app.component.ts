da26150ecc1a30e3becb548c772638c15a3dce65
Charles Liu <dodoinblue@gmail.com>
2017-12-05 20:26:47 -0800

    go to tabs page directly if authenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService } from '../providers/auth/auth.service';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:string = 'LoginPage';
+  rootPage: string;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private auth: AuthService) {
     platform.ready().then(() => {
+
+      this.auth.getAuthenticatedUser().subscribe(auth => {
+        this.rootPage = auth ? 'TabsPage' : 'LoginPage'
+      });
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
